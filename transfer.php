1c686fe60ce13725b27c0c429b191e15c2c5803e
Trophy011 <godswilluzoma517@gmail.com>
2025-07-06 03:08:23 +0100

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -10,7 +10,6 @@ $from_account = $_SESSION['user']['account_number'];
 $to_account = $_POST['to'];
 $amount = (float) $_POST['amount'];
 
-// Check if recipient exists
 $check = $conn->prepare("SELECT * FROM users WHERE account_number=?");
 $check->bind_param("s", $to_account);
 $check->execute();
@@ -24,15 +23,17 @@ if ($_SESSION['user']['balance'] < $amount) {
   die("Insufficient funds.");
 }
 
-// Deduct from sender
 $conn->query("UPDATE users SET balance = balance - $amount WHERE account_number = '$from_account'");
-// Add to receiver
 $conn->query("UPDATE users SET balance = balance + $amount WHERE account_number = '$to_account'");
-// Log transfer
 $conn->query("INSERT INTO transfers (sender_account, receiver_account, amount) VALUES ('$from_account', '$to_account', '$amount')");
 
-// Update session
 $_SESSION['user']['balance'] -= $amount;
+
+// Email notification to receiver
+$toEmail = $receiver['email'];
+$subject = "Credit Alert";
+$message = "You have received $$amount from account $from_account.\n\nUSB";
+mail($toEmail, $subject, $message);
 
 header("Location: dashboard.php");
 ?>
